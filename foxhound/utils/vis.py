42812b1f5f03d9f6fc542804bb5067c3a2cc5c52
themythicaldrago <ryan.louie@students.olin.edu>
2014-11-29 00:12:32 -0500

    uncommented funcs that use plt

--- foxhound/utils/vis.py
+++ foxhound/utils/vis.py
@@ -1,5 +1,5 @@
 import numpy as np
-# from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt
 from scipy.misc import imsave
 
 def color_weight_vis(w):
@@ -22,36 +22,36 @@ def unit_scale(x, eps=1e-8):
 	x *= 1.0 / (x.max() + eps)
 	return x
 
-# def grayscale_grid_vis(X,show=True,save=False,transform=False):
-# 	ngrid = int(np.ceil(np.sqrt(len(X))))
-# 	npxs = np.sqrt(X[0].size)
-# 	img = np.zeros((npxs*ngrid+ngrid-1,npxs*ngrid+ngrid-1))
-# 	for i,x in enumerate(X):
-# 		j = i%ngrid
-# 		i = i/ngrid
-# 		if transform:
-# 			x = transform(x)
-# 		img[i*npxs+i:(i*npxs)+npxs+i,j*npxs+j:(j*npxs)+npxs+j] = x
-# 	if show:
-# 		plt.imshow(img,cmap='gray')
-# 		plt.show()
-# 	if save:
-# 		imsave(save,img)
-# 	return img
+def grayscale_grid_vis(X,show=True,save=False,transform=False):
+	ngrid = int(np.ceil(np.sqrt(len(X))))
+	npxs = np.sqrt(X[0].size)
+	img = np.zeros((npxs*ngrid+ngrid-1,npxs*ngrid+ngrid-1))
+	for i,x in enumerate(X):
+		j = i%ngrid
+		i = i/ngrid
+		if transform:
+			x = transform(x)
+		img[i*npxs+i:(i*npxs)+npxs+i,j*npxs+j:(j*npxs)+npxs+j] = x
+	if show:
+		plt.imshow(img,cmap='gray')
+		plt.show()
+	if save:
+		imsave(save,img)
+	return img
 
-# def color_grid_vis(X,show=True,save=False,transform=False):
-# 	ngrid = int(np.ceil(np.sqrt(len(X))))
-# 	npxs = np.sqrt(X[0].size/3)
-# 	img = np.zeros((npxs*ngrid+ngrid-1,npxs*ngrid+ngrid-1,3))
-# 	for i,x in enumerate(X):
-# 		j = i%ngrid
-# 		i = i/ngrid
-# 		if transform:
-# 			x = transform(x)
-# 		img[i*npxs+i:(i*npxs)+npxs+i,j*npxs+j:(j*npxs)+npxs+j] = x
-# 	if show:
-# 		plt.imshow(img,interpolation='nearest')
-# 		plt.show()
-# 	if save:
-# 		imsave(save,img)
-# 	return img
+def color_grid_vis(X,show=True,save=False,transform=False):
+	ngrid = int(np.ceil(np.sqrt(len(X))))
+	npxs = np.sqrt(X[0].size/3)
+	img = np.zeros((npxs*ngrid+ngrid-1,npxs*ngrid+ngrid-1,3))
+	for i,x in enumerate(X):
+		j = i%ngrid
+		i = i/ngrid
+		if transform:
+			x = transform(x)
+		img[i*npxs+i:(i*npxs)+npxs+i,j*npxs+j:(j*npxs)+npxs+j] = x
+	if show:
+		plt.imshow(img,interpolation='nearest')
+		plt.show()
+	if save:
+		imsave(save,img)
+	return img
